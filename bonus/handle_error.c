51414fd9e3cc4f6806f358e0bef501ae662d78a7
sh1hvb <medchihab651@protonmail.com>
2024-03-11 11:06:49 +0000

    finished

--- bonus/handle_error.c
+++ bonus/handle_error.c
@@ -6,7 +6,7 @@
 /*   By: mchihab <mchihab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/10 14:47:23 by mchihab           #+#    #+#             */
-/*   Updated: 2024/03/10 14:48:00 by mchihab          ###   ########.fr       */
+/*   Updated: 2024/03/11 11:03:02 by mchihab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,17 @@ void	handle_error(char *msg, int ex)
 {
 	perror(msg);
 	exit(WEXITSTATUS(ex));
+}
+
+void	ft_close(int *fds)
+{
+	int	status;
+
+	close(fds[0]);
+	close(fds[1]);
+	while (waitpid(-1, &status, 0) != -1)
+	{
+		if (WEXITSTATUS(status) == 127 || WEXITSTATUS(status) == 1)
+			exit(WEXITSTATUS(status));
+	}
 }
