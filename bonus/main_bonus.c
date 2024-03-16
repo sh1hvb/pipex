984dd83530aa65520e621873e9b9828fa2971d99
Sh1hvb <medchihab651@protonmail.com>
2024-03-16 12:43:00 +0000

    Update main_bonus.c

--- bonus/main_bonus.c
+++ bonus/main_bonus.c
@@ -6,7 +6,7 @@
 /*   By: mchihab <mchihab@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/27 20:35:54 by mchihab           #+#    #+#             */
-/*   Updated: 2024/03/14 12:04:05 by mchihab          ###   ########.fr       */
+/*   Updated: 2024/03/10 15:04:17 by mchihab          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,15 +18,15 @@ void	setup_input_output(int ac, char *av[], int *fd_in, int *fd_out)
 	{
 		if (ac < 6)
 			handle_error("./pipex fd_in cmd1 cmd2 fd_out\n", 1);
-		*fd_out = open(av[ac - 1], O_CREAT | O_RDWR | O_TRUNC, 0666);
+		*fd_out = open(av[ac - 1], O_CREAT | O_RDWR | O_APPEND, 0644);
 		if (*fd_out < 0)
-			handle_error("can't open file", 127);
+			handle_error("can't open file", 1);
 		herdoc(av);
 	}
 	else
 	{
 		*fd_in = open(av[1], O_RDONLY);
-		*fd_out = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0666);
+		*fd_out = open(av[ac - 1], O_CREAT | O_WRONLY | O_TRUNC, 0644);
 		if (*fd_out < 0 || *fd_in < 0)
 			handle_error("error open out_file | in_file", 127);
 		dup2(*fd_in, 0);
@@ -35,7 +35,9 @@ void	setup_input_output(int ac, char *av[], int *fd_in, int *fd_out)
 
 void	create_pipes_and_execute(int ac, char *av[], char *env[], int fd_out)
 {
-	int i, (pid);
+	int	status;
+
+	int i, pid, id;
 	if (ft_strcmp(av[1], "here_doc") == 0)
 		i = 3;
 	else
@@ -48,9 +50,16 @@ void	create_pipes_and_execute(int ac, char *av[], char *env[], int fd_out)
 		dup2(fd_out, 1);
 		exec(av[ac - 2], env);
 	}
-	close(fd_out);
+	id = fork();
+	if (!id)
+		close(fd_out);
+	else
+		while (waitpid(-1, &status, 0) != -1)
+		{
+			if (WEXITSTATUS(status) == 127 || WEXITSTATUS(status) == 1)
+				exit(WEXITSTATUS(status));
+		};
 }
-
 void	here_doc_puts(char **av, int *fdp)
 {
 	char	*line;
@@ -61,7 +70,7 @@ void	here_doc_puts(char **av, int *fdp)
 	{
 		write(1, "heredoc> ", 9);
 		line = get_next_line(0);
-		if (!line || ft_strncmp(line, delimiter,ft_strlen(line)) == 0)
+		if (!line || strcmp(line, delimiter) == 0)
 		{
 			free(delimiter);
 			delimiter = NULL;
@@ -72,6 +81,8 @@ void	here_doc_puts(char **av, int *fdp)
 		ft_putstr_fd(line, fdp[1]);
 		free(line);
 	}
+	close(fdp[1]);
+	free(delimiter);
 }
 
 void	herdoc(char **av)
@@ -98,10 +109,9 @@ int	main(int ac, char *av[], char *env[])
 	int	status;
 
 	if (ac < 5 || !(*av))
-	{
-		perror("not vaild");
-		exit(1);
-	}
+		handle_error("invalid input! ?", 1);
+	if (!(env))
+		handle_error("cannot find the envirenments", 127);
 	setup_input_output(ac, av, &fd_in, &fd_out);
 	create_pipes_and_execute(ac, av, env, fd_out);
 	close(fd_in);
